bf9615c36c6ad2954ca44162257df8bc0ff0c281
Eric Rauchenstein <angryrainbowunicorn@gmail.com>
2025-01-31 15:17:24 +0100

    unfucked it

--- backend-api/wait-for-it.sh
+++ backend-api/wait-for-it.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+
+host="$1"
+shift
+cmd="$@"
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
@@ -7,4 +11,4 @@ until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER"
 done
 
 >&2 echo "Postgres is up - executing command"
-exec uvicorn src.main:app --host 0.0.0.0 --port 8080 --reload
+exec $cmd
