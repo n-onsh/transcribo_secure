8d11495a90c86aeed34fc342a44f7ff682779cde
Eric Rauchenstein <angryrainbowunicorn@gmail.com>
2025-01-31 18:36:37 +0100

    unfucked it

--- backend-api/wait-for-it.sh
+++ backend-api/wait-for-it.sh
@@ -1,14 +1,46 @@
 #!/bin/sh
-set -e
 
-host="$1"
-shift
-cmd="$@"
+# Original version: https://github.com/vishnubob/wait-for-it/blob/master/wait-for-it.sh
+# Usage: ./wait-for-it.sh host:port [-s] [-t timeout] [-- command args]
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+TIMEOUT=15
+STRICT=0
+while :; do
+  case "$1" in
+    *:* )
+    HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
+    PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
+    shift 1
+    ;;
+    -s)
+    STRICT=1
+    shift 1
+    ;;
+    -t)
+    TIMEOUT="$2"
+    shift 2
+    ;;
+    --)
+    shift
+    break
+    ;;
+    *)
+    break
+    ;;
+  esac
+done
+
+for i in `seq $TIMEOUT` ; do
+  nc -z "$HOST" "$PORT" > /dev/null 2>&1
+  result=$?
+  if [ $result -eq 0 ] ; then
+    if [ $# -gt 0 ] ; then
+      exec "$@"
+    fi
+    exit 0
+  fi
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
-exec $cmd
+echo "Timeout occurred after waiting $TIMEOUT seconds for $HOST:$PORT" >&2
+exit 1
